2c124c3a262172ec4284c67b12130e38b4414dd8
adelsanchez67 <adelsanchez67@yahoo.es>
2019-06-18 09:02:49 -0400

    18062019

--- Reportes prestamo/cuotas por empleador.sql
+++ Reportes prestamo/cuotas por empleador.sql
@@ -1,12 +1,10 @@
 SELECT c.`Nombre`, p.inicial cuota FROM prestamo p, cliente c
 WHERE p.cliente=c.numero
 AND p.estatus IN('original','abonado') 
-AND p.sepaga='diario'
-AND p.`Vendedor`=7
-
-
+-- AND p.sepaga='quincenal'
+-- AND p.`Vendedor`=7
  -- clientes buenos
--- AND c.empleador=7
+AND c.empleador=6
 
 
 
