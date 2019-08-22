962662238dc7dbdca506655dc885ee7ad54cf907
adelsanchez67 <adelsanchez67@yahoo.es>
2019-08-22 09:07:53 -0400

    220819-1

---
+++ Reportes prestamo/pendiente por empleador.sql
@@ -0,0 +1,17 @@
+
+SELECT
+e.nombre,
+b.`Nombre`,
+COUNT(c.numero) AS Cuotas,
+p.monto AS Prestamo,
+(SELECT SUM(totalpagado) FROM maestrodepago WHERE Prestamo=p.Numero GROUP BY Prestamo) AS Pago,
+(SELECT SUM(totalpagado) FROM maestrodepago WHERE Prestamo=p.Numero GROUP BY Prestamo) -p.monto AS Resta
+FROM prestamo p, cuota c, cliente b, empleador e
+WHERE p.`Numero`=c.`Prestamo` AND b.empleador=e.numero
+AND p.`Cliente`=b.`Numero`
+AND p.`Estatus` IN('original','abonado')
+AND c.`CapitalPendiente`>1
+ AND e.numero6
+GROUP BY p.`Numero`
+ORDER BY e.nombre, p.monto DESC
+,b.`Nombre`
